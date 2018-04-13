89231d32bfb61546396e87cdade0836f217ca645
Arseni Dudko <arsenidudko@mail.ru>
2018-04-13 17:03:50 +0300

    Fixed k8_install_centos script

--- Install/k8s_intall_centos.sh
+++ Install/k8s_intall_centos.sh
@@ -39,7 +39,7 @@ yum -y install docker
 systemctl enable docker && systemctl start docker
 
 # Add kubernetes repo
-cat < /etc/yum.repos.d/kubernetes.repo
+cat <EOF> /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
